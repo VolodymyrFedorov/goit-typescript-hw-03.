d8e83893d654757471915dc97f6fea0f7a88ee8b
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-09-21 18:38:46 +0300

    1-st

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,41 @@
+class Key {
+  private signature: string;
+  constructor() {
+    this.signature = (Math.random() * 10).toFixed(4);
+  }
+  getSignature(): string {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean = false;
+  key: Key;
+  private tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+  public comeIn(person: Person): undefined | Person[] {
+    if (!this.door) return;
+    this.tenants.push(person);
+  }
+  public abstract openDoor(key: Key): boolean;
+}
+class MyHouse extends House {
+  public openDoor(key: Key): boolean {
+    return (this.door =
+      key.getSignature() === this.key.getSignature() ? true : false);
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +44,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
